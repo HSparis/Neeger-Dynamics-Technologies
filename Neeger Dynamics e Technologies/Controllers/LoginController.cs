038d19fa3ab0bf357c0172868bb2a58f889356f8
Gabriel Alves da Silva <b.reis51@yahoo.com.br>
2023-04-08 11:24:41 -0300

    First Deploy

---
+++ Neeger Dynamics e Technologies/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neeger_Dynamics_e_Technologies.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
